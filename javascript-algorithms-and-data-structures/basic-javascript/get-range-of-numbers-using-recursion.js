fa49d7b14d42455f67aa52887eff887756ad6f3c
Ahmed AbdElLatif <49005074+theahmedlatif@users.noreply.github.com>
2022-12-12 10:03:10 +0200

    Create get-range-of-numbers-using-recursion.js

---
+++ javascript-algorithms-and-data-structures/basic-javascript/get-range-of-numbers-using-recursion.js
@@ -0,0 +1,11 @@
+function rangeOfNumbers(startNum, endNum) {
+  if (endNum < startNum) {
+    return [];
+  } else {
+    const countArray = rangeOfNumbers(startNum, endNum - 1);
+    countArray.push(endNum);
+    return countArray;
+  }
+};
+
+console.log(rangeOfNumbers(3,8));
