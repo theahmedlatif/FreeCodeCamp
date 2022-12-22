b1e6bc8970810e06af9b84c493b0d6e183dc42eb
Ahmed AbdElLatif <49005074+theahmedlatif@users.noreply.github.com>
2022-12-22 10:27:28 +0200

    Create listGenrator.js

---
+++ javascript-algorithms-and-data-structures/ES6/listGenrator.js
@@ -0,0 +1,20 @@
+ const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["no-extra-semi", "no-dup-keys"]
+};
+function makeList(arr) {
+  // Only change code below this line
+  const failureItems = [];
+  arr.forEach(myListGenrator);
+
+  function myListGenrator (value, index, array) {
+    failureItems.push(`<li class="text-warning">${value}</li>`);
+  }
+  // Only change code above this line
+
+  return failureItems;
+}
+
+const failuresList = makeList(result.failure);
+console.log(failuresList);
